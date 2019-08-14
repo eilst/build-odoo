675eafa74f6da4664680a79a1cf448529a03a2b5
Luis <eilstdq@gmail.com>
2019-08-14 09:58:01 -0400

    [ADD] working dockerfile

--- checkout.sh
+++ checkout.sh
@@ -1,15 +1,17 @@
-#!/bin/bash
-# input="./addons.txt"
-# mkdir -p addons
-# while IFS= read -r line
-# do
-#    repo=$(echo  "$line"| awk '{print $1;}')
-#    dir=$(echo "$repo"| awk -F'/' '{print $NF}')
-#    cd addons
-#    git clone "$repo" && cd "$dir"
-#    commit=$(echo  "$line"| awk '{print $2;}')
-#    git checkout "$commit"
-#    cd ..
-# done < "$input"
-cd addons
+!/bin/bash
+ git config --global user.name 'Luis'
+ git config --global user.email 'luis.garcia@savoirfairelinux.com'
+input="./addons.txt"
+mkdir -p git
+while IFS= read -r line
+do
+   repo=$(echo  "$line"| awk '{print $1;}')
+   dir=$(echo "$repo"| awk -F'/' '{print $NF}')
+   cd git
+   git clone "$repo" && cd "$dir"
+   commit=$(echo  "$line"| awk '{print $2;}')
+   git checkout "$commit"
+   cd ..
+done < "$input"
+cd git
 echo $(find . -maxdepth 2 -type d ! -name '.*' -printf '%f,') 
